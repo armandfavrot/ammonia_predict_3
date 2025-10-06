8cd7839ff503bf148a7d126b6a1354075d31154e
Armand <armand.favrot@inrae.fr>
2025-10-06 17:07:30 +0200

    Linting corrections.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,8 +17,6 @@ release = '0.2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import os
-import sys
 sys.path.insert(0, os.path.abspath('../../src'))
 
 extensions = [
